1b9e9240413776f976dbf34456f0eaf6471aedb0
makkuenon <worleyjeremiah252@gmail.com>
2026-07-16 08:53:39 -0400

    Add layout and panel divs to Multispp.js

---
+++ Multispp.js
@@ -0,0 +1,6 @@
+<div id="layout-grid" class="layout hidden"></div>
+<div id="layout-list" class="layout hidden"></div>
+<div id="layout-dev" class="layout hidden"></div>
+
+<div id="signer-panel" class="card"></div>
+<div id="cert-panel" class="card"></div>
