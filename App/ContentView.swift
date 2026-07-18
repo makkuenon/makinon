6fad3470ca37f3ef65158df1a4c7deb0785ee9eb
makkuenon <worleyjeremiah252@gmail.com>
2026-07-18 09:29:36 -0400

    Create ContentView.swift

---
+++ App/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Text("Makinon iOS Signer")
+                .font(.title)
+                .bold()
+
+            Text("UIKit + SwiftUI hybrid")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+        }
+        .padding()
+    }
+}
